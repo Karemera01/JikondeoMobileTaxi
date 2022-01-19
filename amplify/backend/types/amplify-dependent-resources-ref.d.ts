e3644b90beda3447867011d7cc9c04d9144c91bb
Karemera01 <79425594+Karemera01@users.noreply.github.com>
2022-01-19 16:50:35 -0600

    new commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "jikondeotaxi": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
